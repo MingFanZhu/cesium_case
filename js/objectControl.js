98066f9d9f9bd5ff097a7e5f7c33bc0554b69d97
zhumf <1094178787@qq.com>
2022-03-28 10:36:23 +0800

    编辑轴更改

--- js/objectControl.js
+++ js/objectControl.js
@@ -1,32 +1,17 @@
-function objectControl(viewer, object, center, type, callback) {
+function objectControl(viewer, object, center, callback) {
     this.viewer = viewer;
     this.object = object;
     this.modelMatrix = object.modelMatrix.clone();
     this.modelToWorld = Cesium.Transforms.eastNorthUpToFixedFrame(center);//模型坐标系转世界坐标系
     this.worldToModel = Cesium.Matrix4.inverse(this.modelToWorld, new Cesium.Matrix4);//世界坐标系转模型坐标系
-    switch (type) {
-        case "local": {
-            this.control = new Control(this.viewer, center, primitiveInLocalCoordinateCallback.bind(this));
-            break;
-        }
-        case "world": {
-            this.control = new Control(this.viewer, center, primitiveInWorldCoordinateCallback.bind(this));
-            break;
-        }
-        case "custom": {
-            this.control = new Control(this.viewer, center, callback);
-            break;
-        }
-        default:
-            break;
+    if(callback){
+        this.control = new Control(this.viewer, center, callback);
+    }else{
+        this.control = new Control(this.viewer, center, defaultCallback.bind(this));
     }
 }
 
-function primitiveInLocalCoordinateCallback(transform) {
-    this.object.modelMatrix = Cesium.Matrix4.multiply(this.modelMatrix, transform, new Cesium.Matrix4);//变换
-}
-
-function primitiveInWorldCoordinateCallback(transform) {
+function defaultCallback(transform) {
     var newModelMatrix = Cesium.Matrix4.multiply(this.modelToWorld, transform, new Cesium.Matrix4);
     Cesium.Matrix4.multiply(newModelMatrix, this.worldToModel, newModelMatrix);
     this.object.modelMatrix = Cesium.Matrix4.multiply(newModelMatrix, this.modelMatrix, new Cesium.Matrix4);
